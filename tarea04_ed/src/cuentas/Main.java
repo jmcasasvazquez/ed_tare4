6f1ce12524db695dd7790d8b2b7d8a3fbefca76d
jmcasasvazquez <123814711+xmcv1978@users.noreply.github.com>
2023-02-17 23:40:43 +0100

    3. Introducir el método operativa_cuenta, que englobe las sentencias de la clase Main que operan con el objeto cuenta1

--- tarea04_ed/src/cuentas/Main.java
+++ tarea04_ed/src/cuentas/Main.java
@@ -15,10 +15,15 @@ public class Main {
         CCuenta cuenta1;
         double saldoActual;
 
+        operativa_cuenta();
+    }
+
+    private static void operativa_cuenta() {
+        CCuenta cuenta1;
+        double saldoActual;
         cuenta1 = new CCuenta("Antonio Lopez","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
-
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
